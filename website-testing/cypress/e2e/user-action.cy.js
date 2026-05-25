44d3bd38bb017db8f9708aa4cbaea44653060225
Kim <temporary120304@gmail.com>
2026-05-25 21:26:57 +0800

    ayoko na

---
+++ website-testing/cypress/e2e/user-action.cy.js
@@ -0,0 +1,39 @@
+describe('PUP SIS Login', () => {
+
+  it('fills login form', () => {
+
+    cy.visit('https://sis2.pup.edu.ph/student/')
+
+    // Verify page loaded
+    cy.contains('Sign in to start your session')
+
+    // Student Number
+    cy.get('input[type="text"]')
+      .type('2023-00041-UQ-0')
+
+    // Birth Month dropdown
+    cy.get('select')
+      .eq(0)
+      .select('December')
+
+    // Birth Day dropdown
+    cy.get('select')
+      .eq(1)
+      .select('3')
+
+    // Birth Year dropdown
+    cy.get('select')
+      .eq(2)
+      .select('2004')
+
+    // Password
+    cy.get('input[type="password"]')
+      .type('Dec032004')
+
+    cy.get('input[value="Sign in"]').click()
+
+    cy.contains('Grades').click()
+
+  })
+
+})
